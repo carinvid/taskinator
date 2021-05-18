7f85be34a64088a8fca9315698826c2177817380
Carolina Reedy <carinvid@gmail.com>
2021-05-17 21:25:57 -0600

    add ability to add task

--- Css/Javascript/javascript.js
+++ Css/Javascript/javascript.js
@@ -1,17 +1,15 @@
 console.dir(window.document);
 window.document.querySelector("button").textContent;
 document.querySelector(".btn");
-var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1);
-buttonEl.addEventListener("click", function () {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-});
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 var createTaskHandler = function () {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 };
+
+buttonEl.addEventListener("click", createTaskHandler);
