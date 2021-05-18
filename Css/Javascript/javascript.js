8768058a918a7d09b65a1b1d92b35fcf6eeccb69
Carolina Reedy <carinvid@gmail.com>
2021-05-17 21:18:01 -0600

    addidn dom

---
+++ Css/Javascript/javascript.js
@@ -0,0 +1,17 @@
+console.dir(window.document);
+window.document.querySelector("button").textContent;
+document.querySelector(".btn");
+var buttonE1 = document.querySelector("#save-task");
+console.log(buttonE1);
+buttonEl.addEventListener("click", function () {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+});
+var createTaskHandler = function () {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
